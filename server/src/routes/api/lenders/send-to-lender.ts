6c3b8a64cd7acafaaf6a1aa9967c55d873218d92
werb210 <todd.w@boreal.financial>
2025-11-11 09:12:52 -0700

    Change response message to 'OK' in send-to-lender route

--- server/src/routes/api/lenders/send-to-lender.ts
+++ server/src/routes/api/lenders/send-to-lender.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+
 const router = Router();
 
 router.post("/", (_req, res) => {
-  res.json({ message: "Sent to lender" });
+  res.json({ message: "OK" });
 });
 
 export default router;
