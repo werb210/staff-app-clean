70dc652a9ee6e3a936c7e1c47cd36b12c62f4fee
werb210 <todd.w@boreal.financial>
2025-11-15 11:21:36 -0700

    Fix file name for lendersController.js export

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -5,6 +5,9 @@ export * from "./applicationsController.js";
 export * from "./authController.js";
 export * from "./communicationController.js";
 export * from "./documentsController.js";
+
+// FIX: Correct file name (plural): lendersController.js
 export * from "./lendersController.js";
+
 export * from "./notificationsController.js";
 export * from "./pipelineController.js";
