2f461c4fc2bab5242874aa68b4000943431bbfa9
werb210 <todd.w@boreal.financial>
2025-11-15 11:12:29 -0700

    Create index.ts

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,10 @@
+// Unified controller exports for the Staff Server
+
+export * from "./aiController.js";
+export * from "./applicationsController.js";
+export * from "./authController.js";
+export * from "./communicationController.js";
+export * from "./documentsController.js";
+export * from "./lendersController.js";
+export * from "./notificationsController.js";
+export * from "./pipelineController.js";
