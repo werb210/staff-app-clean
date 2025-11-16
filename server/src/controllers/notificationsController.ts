0b223ea94d84735f7a3ce4b3aea30b8d3a942b7c
werb210 <todd.w@boreal.financial>
2025-11-16 12:48:40 -0700

    Implement notifications controller with list method

---
+++ server/src/controllers/notificationsController.ts
@@ -0,0 +1,10 @@
+// server/src/controllers/notificationsController.ts
+import type { Request, Response } from "express";
+import { notificationsService } from "../services/notificationsService.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const notificationsController = {
+  list: asyncHandler(async (_req: Request, res: Response) => {
+    res.json({ ok: true, data: await notificationsService.list() });
+  }),
+};
