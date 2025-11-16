227b73d6eaf2aef0a4b5856ea035f19022a8b2b5
werb210 <todd.w@boreal.financial>
2025-11-16 12:51:08 -0700

    Add companies routes for fetching all and by ID

---
+++ server/src/routes/companies.routes.ts
@@ -0,0 +1,10 @@
+// server/src/routes/companies.routes.ts
+import { Router } from "express";
+import { companiesController } from "../controllers/companiesController.js";
+
+const router = Router();
+
+router.get("/", companiesController.all);
+router.get("/:id", companiesController.get);
+
+export default router;
