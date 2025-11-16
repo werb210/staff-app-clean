0edcd7a5585fe3c76451289975ade40522107a66
werb210 <todd.w@boreal.financial>
2025-11-16 13:05:38 -0700

    Create index.ts to export controllers

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,8 @@
+// server/src/controllers/index.ts
+
+export * from "./applicationsController.js";
+export * from "./documentsController.js";
+export * from "./lendersController.js";
+export * from "./notificationsController.js";
+export * from "./authController.js";
+export * from "./aiController.js";
