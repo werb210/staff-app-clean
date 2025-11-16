3f18e693cc83ef4527a8ab7e85d4ef235104c5e7
werb210 <todd.w@boreal.financial>
2025-11-16 13:08:56 -0700

    Update asyncHandler.ts

--- server/src/utils/asyncHandler.ts
+++ server/src/utils/asyncHandler.ts
@@ -5,7 +5,7 @@ import type { Request, Response, NextFunction } from "express";
 export function asyncHandler(
   fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
 ) {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return function (req: Request, res: Response, next: NextFunction) {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
 }
