6cea85e5ecb4e8fdd0ad4521380ac3e3f0ff8f1d
werb210 <todd.w@boreal.financial>
2025-11-16 12:59:14 -0700

    Refactor env variables initialization

--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -1,6 +1,7 @@
 // server/src/utils/env.ts
+
 export const env = {
-  DATABASE_URL: process.env.DATABASE_URL ?? "",
-  AZURE_BLOB_CONNECTION: process.env.AZURE_BLOB_CONNECTION ?? "",
-  AZURE_BLOB_CONTAINER: process.env.AZURE_BLOB_CONTAINER ?? "",
+  PORT: Number(process.env.PORT || 5000),
+  DATABASE_URL: process.env.DATABASE_URL || "",
+  AZURE_BLOB_CONNECTION: process.env.AZURE_BLOB_CONNECTION || "",
 };
