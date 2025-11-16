e21f89bafb373e62ad10eab858b5e9395540b9e5
werb210 <todd.w@boreal.financial>
2025-11-16 12:45:53 -0700

    Add applications routes for fetching all and by ID

---
+++ server/src/routes/applications.routes.ts
@@ -0,0 +1,10 @@
+// server/src/routes/applications.routes.ts
+import { Router } from "express";
+import { applicationsController } from "../controllers/applicationsController.js";
+
+const router = Router();
+
+router.get("/", applicationsController.all);
+router.get("/:id", applicationsController.get);
+
+export default router;
