c073020dddf9fcff08bfd1667991941d75abe268
werb210 <todd.w@boreal.financial>
2025-11-17 11:44:35 -0700

    Refactor notificationsController to use list function

--- server/src/controllers/notificationsController.ts
+++ server/src/controllers/notificationsController.ts
@@ -1,11 +1,14 @@
 // server/src/controllers/notificationsController.ts
+
 import type { Request, Response } from "express";
 import notificationsService from "../services/notificationsService.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+const list = asyncHandler(async (_req: Request, res: Response) => {
+  const data = await notificationsService.list();
+  res.json({ ok: true, data });
+});
+
 export const notificationsController = {
-  list: asyncHandler(async (_req: Request, res: Response) => {
-    const data = await notificationsService.list();
-    res.json({ ok: true, data });
-  }),
+  list,
 };
