0a6748f61cbbb74105e3b49df8d585736007b25a
werb210 <todd.w@boreal.financial>
2025-11-17 11:00:30 -0700

    Refactor notificationsController to store service result

--- server/src/controllers/notificationsController.ts
+++ server/src/controllers/notificationsController.ts
@@ -1,10 +1,12 @@
 // server/src/controllers/notificationsController.ts
+
 import type { Request, Response } from "express";
 import { notificationsService } from "../services/notificationsService.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const notificationsController = {
   list: asyncHandler(async (_req: Request, res: Response) => {
-    res.json({ ok: true, data: await notificationsService.all() });
+    const rows = await notificationsService.all();
+    res.json({ ok: true, data: rows });
   }),
 };
