34f0fe5717687af1a282eb45b5cad2f8f600efd3
werb210 <todd.w@boreal.financial>
2025-11-17 12:10:06 -0700

    Update siloGuard.ts

--- server/src/middlewares/siloGuard.ts
+++ server/src/middlewares/siloGuard.ts
@@ -1,21 +1,20 @@
+// server/src/middlewares/siloGuard.ts
 import type { Request, Response, NextFunction } from "express";
 
-// Add TS support for req.silo
 declare module "express-serve-static-core" {
   interface Request {
     silo?: string;
   }
 }
 
-export const siloGuard = (req: Request, _res: Response, next: NextFunction) => {
+export default function siloGuard(
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) {
   const silo = req.params.silo;
 
-  if (!silo) {
-    return next(new Error("Silo missing from route."));
-  }
-
   req.silo = silo;
-  next();
-};
 
-export default siloGuard;
+  next();
+}
