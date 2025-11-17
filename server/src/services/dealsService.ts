d3c1b7335ee60e4e5c6dca9c56cb4f1f5a5bd98d
werb210 <todd.w@boreal.financial>
2025-11-17 12:27:50 -0700

    Modify all method to return empty array
    Updated all method to return an empty array instead of querying the database.

--- server/src/services/dealsService.ts
+++ server/src/services/dealsService.ts
@@ -1,10 +1,8 @@
 // server/src/services/dealsService.ts
-import db, { registry } from "../db/registry.js";
+import { registry } from "../db/registry.js";
 
 export const dealsService = {
   async all() {
-    return db.select().from(registry.deals);
+    return [];
   },
 };
-
-export default dealsService;
