a4f2e03479a24d740623264b63329dfa4e941e5b
werb210 <todd.w@boreal.financial>
2025-11-17 11:48:21 -0700

    Refactor notificationsController to inline list method

--- server/src/controllers/notificationsController.ts
+++ server/src/controllers/notificationsController.ts
@@ -1,14 +1,11 @@
 // server/src/controllers/notificationsController.ts
-
 import type { Request, Response } from "express";
 import notificationsService from "../services/notificationsService.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const list = asyncHandler(async (_req: Request, res: Response) => {
-  const data = await notificationsService.list();
-  res.json({ ok: true, data });
-});
-
 export const notificationsController = {
-  list,
+  list: asyncHandler(async (_req: Request, res: Response) => {
+    const data = await notificationsService.list();
+    res.json({ ok: true, data });
+  }),
 };
