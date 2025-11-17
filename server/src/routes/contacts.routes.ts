be02a3f4dad3cead7a2dceb05a4f90db4406850a
werb210 <todd.w@boreal.financial>
2025-11-17 15:24:19 -0700

    Add contacts routes for CRUD operations

---
+++ server/src/routes/contacts.routes.ts
@@ -0,0 +1,13 @@
+// server/src/routes/contacts.routes.ts
+import { Router } from "express";
+import { contactsController } from "../controllers/contactsController.js";
+
+const router = Router();
+
+router.get("/", contactsController.list);
+router.get("/:id", contactsController.get);
+router.post("/", contactsController.create);
+router.put("/:id", contactsController.update);
+router.delete("/:id", contactsController.remove);
+
+export default router;
