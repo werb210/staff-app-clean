20513c3458a8ae8d8c07c83e940ca83850924467
werb210 <todd.w@boreal.financial>
2025-11-17 11:01:29 -0700

    Refactor notifications service to use 'all' method

--- server/src/services/notificationsService.ts
+++ server/src/services/notificationsService.ts
@@ -1,14 +1,13 @@
 // server/src/services/notificationsService.ts
+
 import { emailService } from "./emailService.js";
 import { smsService } from "./smsService.js";
 
-async function list() {
-  return {
-    sms: await smsService.list(),
-    emails: await emailService.list(),
-  };
-}
-
-export default {
-  list,
+export const notificationsService = {
+  async all() {
+    return {
+      sms: await smsService.list(),
+      emails: await emailService.list(),
+    };
+  },
 };
