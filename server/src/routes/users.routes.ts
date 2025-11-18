9a00ae4d39ba457880fc7bdc94d1ce18fc591c8e
werb210 <todd.w@boreal.financial>
2025-11-18 09:35:44 -0700

    Replace userController with usersController

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,12 +1,13 @@
+// server/src/routes/users.routes.ts
 import { Router } from "express";
-import { userController } from "../controllers/userController.js";
+import { usersController } from "../controllers/usersController.js";
 
 const router = Router();
 
-router.get("/", userController.list);
-router.get("/:id", userController.get);
-router.post("/", userController.create);
-router.put("/:id", userController.update);
-router.delete("/:id", userController.remove);
+router.get("/", usersController.list);
+router.get("/:id", usersController.get);
+router.post("/", usersController.create);
+router.put("/:id", usersController.update);
+router.delete("/:id", usersController.remove);
 
 export default router;
