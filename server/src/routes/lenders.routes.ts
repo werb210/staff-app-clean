0fa08db84354b5fa01ebc0a70883fee46c9c6ee7
werb210 <todd.w@boreal.financial>
2025-11-18 09:40:24 -0700

    Add CRUD routes for lenders

--- server/src/routes/lenders.routes.ts
+++ server/src/routes/lenders.routes.ts
@@ -1,10 +1,13 @@
 // server/src/routes/lenders.routes.ts
-
 import { Router } from "express";
 import { lendersController } from "../controllers/lendersController.js";
 
 const router = Router();
 
 router.get("/", lendersController.list);
+router.get("/:id", lendersController.get);
+router.post("/", lendersController.create);
+router.put("/:id", lendersController.update);
+router.delete("/:id", lendersController.remove);
 
 export default router;
