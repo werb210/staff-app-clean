b9a407d5e37ad3bfbdb33d2617080103336e0c79
werb210 <todd.w@boreal.financial>
2025-12-01 16:24:05 -0700

    Update companies.routes.ts

--- server/src/routes/companies.routes.ts
+++ server/src/routes/companies.routes.ts
@@ -1,10 +1,10 @@
-// server/src/routes/companies.routes.ts
 import { Router } from "express";
-import { companiesController } from "../controllers/companiesController.js";
+import companiesController from "../controllers/companiesController.js";
 
 const router = Router();
 
+// Only list + create exist
 router.get("/", companiesController.list);
-router.get("/:id", companiesController.get);
+router.post("/", companiesController.create);
 
 export default router;
